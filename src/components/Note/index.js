7577c6ae1ba867fbd1b0e0c02c80594c2e877e05
Dor Sarel <Dor.Sarel@wdc.com>
2020-01-06 08:52:49 +0200

    add delete & edit button to note

--- src/components/Note/index.js
+++ src/components/Note/index.js
@@ -3,7 +3,13 @@ import './style.scss';
 
 export const Note = ({ title, body }) => (
   <div className='note'>
-    <div className='note__header'>{title}</div>
+    <div className='note__header'>
+      <h5>{title}</h5>
+      <div className='header__buttons'>
+        <button className='btn btn--delete'>Delete</button>
+        <button className='btn btn--edit'>Edit</button>
+      </div>
+    </div>
     <div className='note__body'>{body}</div>
   </div>
 );
